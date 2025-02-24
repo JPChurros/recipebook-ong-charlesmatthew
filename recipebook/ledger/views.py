878ec9189e625f337d93d5db090952cac5a2f56a
JPChurros <151139142+JPChurros@users.noreply.github.com>
2025-02-24 14:50:21 +0800

    [recipes] Recipes are completed with proper django loops Pork Adobo and Sinigang are added
    Signed-off-by: JPChurros <151139142+JPChurros@users.noreply.github.com>

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -9,11 +9,30 @@ def recipe_list(request):
 def recipeListTemplate(request, num=1):
     if num == 1:
         number = "1"
+        recipes = [
+                "Tomato - 3 pieces",
+                "Onion - 1 piece",
+                "Pork - 1 kilo",
+                "Water - 1 liter",
+                "Sinigang Mix - 1 packet",
+        ]
     elif num == 2:
         number = "2"
+        recipes = [
+                "garlic - 1 head",
+                "onion - 1 piece",
+                "vinegar - 1/2 cup",
+                "water - 1 cup",
+                "salt - 1 tablespoon",
+                "whole black peppers - 1 tablespoon",
+                "pork - 1 kilo",
+        ]
     else:
         number = "0"
-    context = {"number": number}
+        recipes = [
+                "Wrong Page 404"
+        ]
+    context = {"number": number, "recipes": recipes}
     return render(request, 'recipeListTemplate.html', context)
 
 # Create your views here.
