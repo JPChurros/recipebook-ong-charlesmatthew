5ccd57577ff4bafed8c363035a6d6f03e6eee7fb
JPChurros <151139142+JPChurros@users.noreply.github.com>
2025-02-24 14:47:45 +0800

    [recipes] Linked recipe 1 and 2 to recipe list Created template for recipe with variables added int and context to both recipes updated view and urls
    Signed-off-by: JPChurros <151139142+JPChurros@users.noreply.github.com>

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = {
     path('recipes/list/', recipe_list, name='recipe_list'),
+    path('recipe/<int:num>/', recipeListTemplate, name='recipeListTemplate'),
 }
 
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -6,4 +6,14 @@ def recipe_list(request):
     template = loader.get_template('homepage.html')
     return HttpResponse(template.render())
 
+def recipeListTemplate(request, num=1):
+    if num == 1:
+        number = "1"
+    elif num == 2:
+        number = "2"
+    else:
+        number = "0"
+    context = {"number": number}
+    return render(request, 'recipeListTemplate.html', context)
+
 # Create your views here.
