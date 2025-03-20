83aaae8b4b599ec40c5f22d95e2a5c3334a20e4e
JPChurros <151139142+JPChurros@users.noreply.github.com>
2025-03-20 18:04:18 +0800

    [models.py] Recipe class updated to record author, date created, and date updated.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -12,6 +12,10 @@ class Profile(models.Model):
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.name
