d3c988046650680aa6d90143b7ff7a89705fd55b
JPChurros <151139142+JPChurros@users.noreply.github.com>
2025-03-20 18:05:24 +0800

    [models.py] ACTUAL COMMITED RECIPE UPDATES

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -12,10 +12,6 @@ class Profile(models.Model):
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
-
 
     def __str__(self):
         return self.name
@@ -25,7 +21,10 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100, unique=True)
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return self.name
 
